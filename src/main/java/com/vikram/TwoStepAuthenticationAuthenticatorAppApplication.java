d58f86a8f66f075cb614de3d47ddb4be07837b96
Vikram <vikky5vikram@gmail.com>
2024-03-07 12:02:09 +0530

    Initial Commit

---
+++ src/main/java/com/vikram/TwoStepAuthenticationAuthenticatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.vikram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoStepAuthenticationAuthenticatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoStepAuthenticationAuthenticatorAppApplication.class, args);
+	}
+
+}
